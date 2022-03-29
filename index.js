ebb0177c7812a9be52901e60b8853dc5f5b8bb5a
Danny Rodriguez <danny007111@hotmail.com>
2022-03-29 14:47:59 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function superbowlWin(record){
+
+    function superbowl(year){
+        return year.result === "W";
+        // if(year.result === "W"){
+        // return true;
+        // }else{
+        // return false;
+        // }
+    };
+  
+    const winningYearObject = record.find(superbowl);
+
+    if(winningYearObject){
+        return winningYearObject.year;
+    };
+    
+};
